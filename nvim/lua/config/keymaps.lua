78ebdec312b3304ccce29ba71479e268415af007
nan <nan20110923@outlook.com>
2025-01-14 15:29:11 +0800

    + alacritty

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- Fzf lua
+map("n", "<C-f>", "<cmd>FzfLua files<cr>", { desc = "Search files" })
--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 DOTFILES_PATH=$(dirname $(realpath "$0"))
 
+# alacritty
+ln -fs $DOTFILES_PATH/alacritty $HOME/.config
+
+# nvim
 ln -fs $DOTFILES_PATH/nvim $HOME/.config
